2eb25e265eed00d7d542a1c88bc06a500956d674
cristina-mendoza <31021876+cristina-mendoza@users.noreply.github.com>
2017-08-29 20:06:11 -0500

    Add files via upload
    programa de conteo de agua en c#  blanca Guadalupe delgado valencia y cristina cecilia mendoza mercado

--- Program.cs
+++ Program.cs
@@ -1,32 +1,32 @@
 ﻿using System;
-
-namespace agua
+class program
 {
-    class Program
+ static void Main()
     {
-        static void Main(string[] args)
+      
+        Console.WriteLine("el programon");
+        int min, fecha, min2 = 0;
+        for (int i = 1; i <= 1; i++)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("¿cuantos minutos tardas en bañarte al dia?:");
+            min = Convert.ToInt32(Console.ReadLine());
+            min2 = min * 12;
+            Console.WriteLine("el agua que gastas al dia equivale a esta cantidad de botellas de agua:" + min2);
+            Console.ReadKey();
+
+            Console.WriteLine("¿cual es tu fecha de cumpleaños?:(yyyymmdd)");
+            fecha = Convert.ToInt32(Console.ReadLine());
+           
+            DateTime hoy = DateTime.Today;
+            DateTime cumple =  new DateTime(2018,01,19);
+            TimeSpan diaEntre = cumple - hoy;
+            System.Console.WriteLine(diaEntre.TotalDays);
+            double  promedio;
+            promedio = diaEntre.TotalDays * min2;
+            Console.WriteLine("el resultado de botellas que gastarias asta tu cumpleaños es de:" + promedio);
+          Console.ReadKey();
 
         }
-
-
-
-        ///<summary>
-        ///Calcula el número de botella dependiendo de los minutos 
-        ///cada minuto equivale a 12 botellas
-        ///</summary>
-        static int calcularBotella(int minutos)
-        {
-            return 0;
-        }
-
-        ///<summary>
-        ///Calcula el número de días restantes hasta la fecha dada
-        ///</summary>
-        static int calcularDiasRestantes(DateTime fechaCumple)
-        {
-            return 0;
-        }
+        
     }
 }
